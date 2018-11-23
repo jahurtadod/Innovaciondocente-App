0520ce4b48e757e52d199c23fa85957d5e491d89
utpl <bruno.be81@gmail.com>
2018-11-23 17:55:16 -0500

    feat(Big card): add desing

---
+++ lib/pages/observatorio-edutendencias/noticias/noticias-page.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:innovaciondocente_app/classes/noticia.dart';
+import 'package:innovaciondocente_app/pages/observatorio-edutendencias/noticias/asymmetric-view.dart';
+
+class NoticiasPage extends StatefulWidget {
+  final Stream<List> stream;
+
+  NoticiasPage({this.stream});
+
+  @override
+  _NoticiasPageState createState() => _NoticiasPageState();
+}
+
+class _NoticiasPageState extends State<NoticiasPage> {
+  List<Noticia> _noticias;
+  StreamSubscription<List<Noticia>> _subs;
+
+  @override
+  void initState() {
+    super.initState();
+    this._subs = widget.stream.listen((noticias) {
+      setState(() {
+        this._noticias = noticias;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    this._subs.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Noticias"),
+      ),
+      body: (this._noticias == null)
+          ? Text("Loading")
+          : AsymmetricView(noticias: this._noticias),
+    );
+  }
+}
--- lib/pages/observatorio-edutendencias/noticias/single-news-column.dart
+++ lib/pages/observatorio-edutendencias/noticias/single-news-column.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:innovaciondocente_app/classes/filters.dart';
 import 'package:innovaciondocente_app/classes/noticia.dart';
 
 class SingleNewsColumn extends StatelessWidget {
@@ -11,24 +12,69 @@ class SingleNewsColumn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: InkWell(
-        onTap: () {},
-        child: Container(
-          padding: EdgeInsets.all(10.0),
-          child: Column(
-            children: <Widget>[
-              Image.network(noticia.img),
-              Text(
-                noticia.name,
-                style: Theme.of(context).textTheme.title,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          child: Card(
+            child: InkWell(
+              onTap: () {},
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    /// Image asset
+                    _buildImage(context),
+
+                    /// Details
+                    _buildDetails(context)
+                  ],
+                ),
               ),
-              SizedBox(height: 20.0),
-              Text(noticia.description),
-            ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column _buildDetails(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 10.0),
+        Text(
+          noticia.name,
+          style: Theme.of(context).textTheme.title,
+        ),
+        SizedBox(height: 5.0),
+        Text(
+          Filters.date(noticia.created),
+          style: Theme.of(context).textTheme.overline,
+        ),
+        SizedBox(height: 10.0),
+        Text(noticia.description),
+        SizedBox(height: 10.0),
+      ],
+    );
+  }
+
+  Widget _buildImage(BuildContext context) {
+    return AspectRatio(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.all(
+            Radius.circular(3.0),
+          ),
+          image: DecorationImage(
+            image: NetworkImage(noticia.img),
+            fit: BoxFit.cover,
           ),
         ),
       ),
+      aspectRatio: 1,
     );
   }
 }
