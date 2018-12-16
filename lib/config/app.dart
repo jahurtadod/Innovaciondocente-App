62926847aab00dcf9db6c73a10cbcc547a782d89
baesparza <bruno.be81@gmail.com>
2018-12-15 23:38:11 -0500

    feat(notifications): add config

--- lib/config/app.dart
+++ lib/config/app.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:innovaciondocente_app/config/routes.dart';
 import 'package:innovaciondocente_app/config/theme.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class InnovaciondocenteApp extends StatelessWidget {
+class InnovaciondocenteApp extends StatefulWidget {
+  @override
+  InnovaciondocenteAppState createState() {
+    return new InnovaciondocenteAppState();
+  }
+}
+
+class InnovaciondocenteAppState extends State<InnovaciondocenteApp> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  @override
+  void initState() {
+    super.initState();
+    this._firebaseMessaging.getToken().then(print);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
